6d61f98ef11c4fcdfd30135564a9290d7337ca27
Brian McGillion <brian.mcgillion@darkmatter.ae>
2020-05-13 12:57:12 +0400

    Add org-ref package for citations and bibliography
    org-ref allows for easy citation management and provides numerous
    interfaces to online publishing and reference management tools.

--- modules/tools/biblio/packages.el
+++ modules/tools/biblio/packages.el
@@ -6,3 +6,7 @@
   (package! ivy-bibtex :pin "94807a3d3419f90b505eddc3272e244475eeb4f2"))
 (when (featurep! :completion helm)
   (package! helm-bibtex :pin "94807a3d3419f90b505eddc3272e244475eeb4f2"))
+(when (featurep! :lang org)
+  (package! org-ref :pin "113506df694d65e065534e516db1c592d61f44b7"))
+(when (featurep! :lang org +roam)
+  (package! org-roam-bibtex :pin "c2b097e982108f53bb995c74dde3b1a9dd28cb5b"))
